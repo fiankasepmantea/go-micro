1565801379b4bc5523ec6b9476f7632db9f27e55
fiankasepmantea <fianet341@gmail.com>
2026-04-29 15:58:30 +0700

    12. Adding a new function in the Broker to log items via RabbitMQ

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"broker/event"
 	"bytes"
 	"encoding/json"
 	"errors"
+	"log"
 	"net/http"
 	"time"
-	"log"
 )
 
 type RequestPayload struct {
@@ -55,7 +56,7 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 		case "auth":
 			app.authenticate(w, requestPayload.Auth)
 		case "log":
-			app.logItem(w, requestPayload.Log)
+			app.logEventViaRabbit(w, requestPayload.Log)
 		case "mail":
 			app.sendEmail(w, requestPayload.Mail)
 		default:
@@ -193,6 +194,36 @@ func (app *Config) sendEmail(w http.ResponseWriter, msg MailPayload) {
 	app.writeJSON(w, http.StatusOK, payload)
 }
 
+func (app *Config) logEventViaRabbit(w http.ResponseWriter, l LogPayload) {
+	err := app.pushToQueue(l.Name, l.Data)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	var payload jsonResponse
+	payload.Message = "logged via RabbitMQ"
+	app.writeJSON(w, http.StatusOK, payload)
+}
+
+func (app *Config) pushToQueue(name, msg string) error {
+	emitter, err := event.NewEventEmitter(app.Rabbit)
+	if err != nil {
+		return err
+	}
+
+	payload := LogPayload{
+		Name: name,
+		Data: msg,
+	}
+
+	j, _ = json.MarshalIndent(&payload, "", "\t")
+	err = emitter.Push(string(j), "log.INFO")
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 
 
